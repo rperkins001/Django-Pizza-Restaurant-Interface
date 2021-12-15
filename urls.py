73abb0c0821e9c2b17cc458e24b58e1582004991
rperkins001 <84806371+rperkins001@users.noreply.github.com>
2021-12-15 11:36:06 +0200

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.homepage, name = 'homepage'),
-    path('order/', views.pizza_order, name = 'pizza_order'),
-    path('checkout/', views.checkout, name = 'checkout'),
-    path('confirm/', views.confirm, name = 'confirm'),
-]
